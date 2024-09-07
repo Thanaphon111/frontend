0eb8f50c7e5552eee038034b688ed8392d2fbea7
Thanaphon210145 <notebew1@gmail.com>
2024-09-07 12:03:39 +0700

    ำก

--- coreui-free-react-admin-template/src/wbconfig.js
+++ coreui-free-react-admin-template/src/wbconfig.js
@@ -1,15 +1,15 @@
 const wbconfig = {
     development: {
-        hosturl  : 'https://lab-parse-server.se-rmutl.net/api',
-        wsurl    : 'wss://lab-parse-server.se-rmutl.net',
+        hosturl  : 'https://10.21.43.203:8605/api',
+        wsurl    : 'wss://10.21.43.203:8605',
         masterKey     : 'wallboardapi',
         clientKey     : 'wallboardapi',
         javascriptKey : 'wallboardapi',
         appId : "wallboardapi"
     },
     production: {
-        hosturl  : 'https://10.21.43.203:8005/api',
-        wsurl    : 'wss://10.21.43.203:8005',
+        hosturl  : 'https://10.21.43.203:8605/api',
+        wsurl    : 'wss://10.21.43.203:8605',
         masterKey     : 'wallboardapi',
         clientKey     : 'wallboardapi',
         javascriptKey : 'wallboardapi',
@@ -17,5 +17,3 @@ const wbconfig = {
     }
 };
 export default wbconfig;
-
-
